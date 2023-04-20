8313e0299215373b884b1f5377fc84536e0e01bb
김희철 <gmlcjf0326@hanmail.net>
2023-04-20 14:40:39 +0900

    feat : Dutsem No li 👩

---
+++ src/main/java/com/example/test2/week1/ScanFomatnig.java
@@ -0,0 +1,14 @@
+package com.example.test2.week1;
+
+import java.util.Scanner;
+
+public class ScanFomatnig {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int val1 = sc.nextInt();
+    int val2 = sc.nextInt();
+
+    System.out.printf("%d 더하기 %d = %d 입니다.",val1, val2, val1+val2);
+  }
+
+}
