53b995bdc15fa7295fa28d621de876746ee6d5d6
김희철 <gmlcjf0326@hanmail.net>
2023-04-20 15:19:32 +0900

    feat:VariableEX👩 None a+b

---
+++ src/main/java/com/example/test2/week1/VariableEx.java
@@ -0,0 +1,17 @@
+package com.example.test2.week1;
+
+public class VariableEx {
+  public static void main(String[] args) {
+    int iVal;
+    iVal = 0; // 최초로 값을 지정하는 것은 초기화입니다.
+    VariableEx printHello; // 변수 선언
+    printHello = new VariableEx(); // new를 이용해 인스턴스화 및 초기화
+
+    System.out.println(iVal);
+    printHello.print();
+  }
+
+  public void print() {
+    System.out.println("Hello, world!");
+  }
+}
