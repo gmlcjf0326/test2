47b239bb6b3e80c46d5514148c893db0b0b907a1
김희철 <gmlcjf0326@hanmail.net>
2023-04-20 10:04:25 +0900

    feat : Code up about 1001~1008

---
+++ src/main/java/com/example/test2/Code100/Main.java
@@ -0,0 +1,141 @@
+package com.example.demo1.codeUpTest.Code100;
+
+import java.util.Scanner;
+
+//public class Main {
+//}
+
+//    1번문제
+//    System.out.println("Hello");
+//    2번문제
+//    System.out.println("Hello World");
+//    3번문제
+//    System.out.println("Hello\nWorld");
+//    4번문제
+//    System.out.println("'Hello'");
+//    5번문제
+//    System.out.println("\"Hello World\"");
+//    6번문제
+//    System.out.println("\"!@#$%^&*()\"");
+//    7번문제
+//    System.out.println("\"C:\\Download\\hello.cpp\"");
+//    8번문제
+//    System.out.printf("\u250c");
+//    System.out.printf("\u252c");
+//    System.out.println("\u2510");
+//    System.out.printf("\u251c");
+//    System.out.printf("\u253c");
+//    System.out.println("\u2524");
+//    System.out.printf("\u2514");
+//    System.out.printf("\u2534");
+//    System.out.printf("\u2518");
+
+
+
+
+
+
+//    9번문제
+//    10번문제
+//    11번문제
+//    12번문제
+//    13번문제
+//    14번문제
+//    15번문제
+//    16번문제
+//    17번문제
+//    18번문제
+//    19번문제
+//    20번문제
+//    21번문제
+//    22번문제
+//    23번문제
+//    24번문제
+//    25번문제
+//    26번문제
+//    27번문제
+//    28번문제
+//    29번문제
+//    30번문제
+//    31번문제
+//    32번문제
+//    33번문제
+//    34번문제
+//    35번문제
+//    36번문제
+//    37번문제
+//    38번문제
+//    39번문제
+//    40번문제
+//    41번문제
+//    42번문제
+//    43번문제
+//    44번문제
+//    45번문제
+//    46번문제
+//    47번문제
+//    48번문제
+//    49번문제
+//    50번문제
+//    51번문제
+//    52번문제
+//    53번문제
+//    54번문제
+//    55번문제
+//    56번문제
+//    57번문제
+//    58번문제
+//    59번문제
+//    60번문제
+//    61번문제
+//    62번문제
+//    63번문제
+//    64번문제
+//    65번문제
+//    66번문제
+//    67번문제
+//    68번문제
+//    69번문제
+//    70번문제
+//    71번문제
+//    72번문제
+//    73번문제
+//    74번문제
+//    75번문제
+//    76번문제
+//    77번문제
+//    78번문제
+//    79번문제
+//    80번문제
+//    81번문제
+//    82번문제
+//    83번문제
+//    84번문제
+//    85번문제
+//    86번문제
+//    87번문제
+//    88번문제
+//    89번문제
+//
+//public class Main {
+//  public static void main(String[] args) {
+//    abcd(1,2,3);
+//  }
+//
+//  public void abcd(int a, int d, int n){   // static이 없으면 인스턴스 메써드,
+//    int result = a + (n-1) * d;
+//    System.out.println(result);
+//  }
+//}
+
+//    90번문제
+//    91번문제
+//    92번문제
+//    93번문제
+//    94번문제
+//    95번문제
+//    96번문제
+//    97번문제
+//    98번문제
+//    99번문제
+//    100번문제
