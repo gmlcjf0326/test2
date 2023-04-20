0719e2a4e915105eb047983cc06ac03457a368a4
김희철 <gmlcjf0326@hanmail.net>
2023-04-20 15:58:11 +0900

    CodeUp ~ 1015👩/Never used int a;

---
+++ src/main/java/com/example/test2/week1/Codeup1010.java
@@ -0,0 +1,12 @@
+// 숫자 입력하여 출력받기.
+
+package com.example.test2.week1;
+
+import java.util.Scanner;
+
+public class Codeup1010 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.printf("%d", sc.nextInt());
+  }
+}
