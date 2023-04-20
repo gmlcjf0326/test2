25c3408c6e3d03cbbb15b1c43d34e40be022faaa
김희철 <gmlcjf0326@hanmail.net>
2023-04-20 15:27:42 +0900

    feat: ScannerAndVariable👩Never a, b

--- src/main/java/com/example/test2/week1/ScannerStrExTest.java
+++ src/main/java/com/example/test2/week1/ScannerStrExTest.java
@@ -1,9 +1,11 @@
 package com.example.test2.week1;
+
+import java.util.Scanner;
+
 public class ScannerStrExTest {
   public static void main(String[] args) {
     ScannerStrEx ss = new ScannerStrEx();
     ss.stringInput();
   }
 }
-
 
