b16062723e32b53b62f21c4f95d58318d949e3ec
김희철 <gmlcjf0326@hanmail.net>
2023-04-26 15:24:22 +0900

    feat : codeUP 1041~1047 clear

--- src/main/java/com/example/test2/Code100/Main.java
+++ src/main/java/com/example/test2/Code100/Main.java
@@ -1,58 +1,52 @@
 package com.example.test2.Code100;
 
+import java.util.Arrays;
 import java.util.Scanner;
-//    31번문제
-//  Scanner sc = new Scanner(System.in);
-//      int a = sc.nextInt();
-//      System.out.printf("%o", a);
-//    32번문제
-//  Scanner sc = new Scanner(System.in);
-//      int a = sc.nextInt();
-//      System.out.printf("%x", a);
-//    33번문제
-//  Scanner sc = new Scanner(System.in);
-//      int a = sc.nextInt();
-//      System.out.printf("%X", a);
-//    34번문제
-//    Scanner sc = new Scanner(System.in);
-//        String octal = sc.next();
-//        int decimal = Integer.parseInt(octal, 8);
-//        System.out.println(decimal);
-//    35번문제
-//    Scanner sc = new Scanner(System.in);
-//        String octal = sc.next();
-//        int decimal = Integer.parseInt(octal, 16);
-//        System.out.printf("%o", decimal);
-//    36번문제
-//    Scanner sc = new Scanner(System.in);
-//        char octal = sc.next().charAt(0);
-//        int asci = (int) octal;
-//        System.out.printf("%d", asci);
-//    37번문제
-//    Scanner sc = new Scanner(System.in);
-//        int desimal = sc.nextInt();
-//        char asci = (char) desimal;
-//        System.out.printf("%c", asci);
-//    38번문제
 public class Main {
+//  23번문제 ~ 30번문제 x
   public static void main(String[] args) {
+
+//    43번문제
+//    Scanner sc = new Scanner(System.in);
+//    int a = sc.nextInt();
+//    int b = sc.nextInt();
+//    int outputNum = a%b;
+//    System.out.printf("%d", outputNum);
+
+//    44번문제
+//    Scanner sc = new Scanner(System.in);
+//    long a = sc.nextLong();
+//    System.out.printf("%d", ++a);
+//    45번문제
+//        Scanner sc = new Scanner(System.in);
+//        int inputNum1 = sc.nextInt();
+//        int inputNum2 = sc.nextInt();
+//        System.out.printf("%d\n", inputNum1+inputNum2);
+//        System.out.printf("%d\n", inputNum1-inputNum2);
+//        System.out.printf("%d\n", inputNum1*inputNum2);
+//        System.out.printf("%d\n", inputNum1/inputNum2);
+//        System.out.printf("%d\n", inputNum1%inputNum2);
+//        System.out.printf("%.2f\n", (float)inputNum1%inputNum2);
+//    46번문제
+//    Scanner sc = new Scanner(System.in);
+//    String[] inputString = sc.nextLine().split(" ");
+//    long inputNum1 = Long.parseLong(inputString[0]);
+//    long inputNum2 = Long.parseLong(inputString[1]);
+//    long inputNum3 = Long.parseLong(inputString[2]);
+//    System.out.printf("%d\n", inputNum1+inputNum2+inputNum3);
+//    System.out.printf("%.1f\n", (float)(inputNum1+inputNum2+inputNum3)/3);
+
     Scanner sc = new Scanner(System.in);
-    int desimal = sc.nextInt();
-    char asci = (char) desimal;
-    System.out.printf("%c", asci);
+    String[] inputString = sc.nextLine().split(" ");
+    long inputNum1 = Long.parseLong(inputString[0]);
+    long inputNum2 = Long.parseLong(inputString[1]);
+    long inputNum3 = Long.parseLong(inputString[2]);
+    System.out.printf("%d\n", inputNum1+inputNum2+inputNum3);
+    System.out.printf("%.1f\n", (float)(inputNum1+inputNum2+inputNum3)/3);
   }
 }
-
-
-//    39번문제
-//    40번문제
-//    41번문제
-//    42번문제
-//    43번문제
-//    44번문제
-//    45번문제
-//    46번문제
 //    47번문제
+
 //    48번문제
 //    49번문제
 //    50번문제
@@ -96,7 +90,7 @@ public class Main {
 //    88번문제
 //    89번문제
 //
-//public class Main {
+// public class Main {
 //  public static void main(String[] args) {
 //    abcd(1,2,3);
 //  }
@@ -122,21 +116,21 @@ public class Main {
 
 
 
-//    1번문제
+//    1번문제 출력하기01(설명)
 //    System.out.println("Hello");
-//    2번문제
+//    2번문제 출력하기02(설명)
 //    System.out.println("Hello World");
-//    3번문제
+//    3번문제 출력하기03(설명)
 //    System.out.println("Hello\nWorld");
-//    4번문제
+//    4번문제 출력하기04(설명)
 //    System.out.println("'Hello'");
-//    5번문제
+//    5번문제 출력하기05(설명)
 //    System.out.println("\"Hello World\"");
-//    6번문제
+//    6번문제 출력하기06(설명)
 //    System.out.println("\"!@#$%^&*()\"");
-//    7번문제
+//    7번문제 출력하기07(설명)
 //    System.out.println("\"C:\\Download\\hello.cpp\"");
-//    8번문제
+//    8번문제 출력하기08(설명)
 //    System.out.printf("\u250c");
 //    System.out.printf("\u252c");
 //    System.out.println("\u2510");
@@ -147,48 +141,169 @@ public class Main {
 //    System.out.printf("\u2534");
 //    System.out.printf("\u2518");
 
-//    10번문제
+//    10번문제 정수 1개 입력받아 그대로 출력하기(설명)
 //    Scanner sc = new Scanner(System.in);
 //    System.out.printf("%d", sc.nextInt());
 
-//    11번문제
+//    11번문제 문자 1개 입력받아 그대로 출력하기(설명)
 //    Scanner sc = new Scanner(System.in);
 //    System.out.printf("%c", sc.next().charAt(0));
 
-//    12번문제
+//    12번문제 실수 1개 입력받아 그대로 출력하기(설명)
 //    Scanner sc = new Scanner(System.in);
 //    System.out.printf("%f", sc.nextFloat());
 
-//    13번문제
+//    13번문제 정수 2개 입력받아 그대로 출력하기(설명)
 //        Scanner sc = new Scanner(System.in);
 //        int a = sc.nextInt();
 //        int b = sc.nextInt();
 //        System.out.printf("%d %d", a, b);
 
-//    14번문제
+//    14번문제 문자 2개 입력받아 순서 바꿔 출력하기(설명)
 //    Scanner sc = new Scanner(System.in);
 //    int val1 = sc.next().charAt(0);
 //    int val2 = sc.next().charAt(0);
 //    System.out.printf("%c %c", val2, val1);
-//    15번문제 완료!
+
+//    15번문제 실수 입력받아 둘째 자리까지 출력하기(설명)
 //    Scanner sc = new Scanner(System.in);
 //    float val1 = sc.nextFloat();
 //    System.out.printf("%.3f", val1);//
-//    17번문제
+
+//    17번문제 정수 1개 입력받아 3번 출력하기(설명)
 //    Scanner sc = new Scanner(System.in);
 //    int val1 = sc.nextInt();
 //    System.out.printf("%d %d %d", val1, val1, val1);
-//    18번문제
-//    public static void main(String[] args) {
+
+//  18번 문제
 //    Scanner sc = new Scanner(System.in);
-//    int data1 = sc.nextInt();
-//    int data2 = sc.nextInt();
-//    System.out.printf("%d:%d", data1, data2);
-//    21, 22번문제
+//    String[] strArr = sc.nextLine().split(":");
+//    int hour = Integer.parseInt(strArr[0]);
+//    int minute = Integer.parseInt(strArr[1]);
+//    System.out.printf("%d:%d", hour, minute);
+//  19번 문제
+//    Scanner sc = new Scanner(System.in);
+//    String[] strArr = sc.nextLine().split("\\.");
+//    int year = Integer.parseInt(strArr[0]);
+//    int month = Integer.parseInt(strArr[1]);
+//    int day = Integer.parseInt(strArr[2]);
+//    System.out.printf("%04d.%02d.%02d", year, month, day);
+//  }
+//}
+//  20번 문제
+//Scanner sc = new Scanner(System.in);
+//  String a = sc.next();
+//  String[] arr = a.split("\\-");
+//    System.out.printf("%s%s", arr[0], arr[1]);
+
+//    21, 22번문제 단어, 문장 1개 입력받아 그대로 출력하기(설명)
 //    Scanner sc = new Scanner(System.in);
 //        String data2 = sc.nextLine();
 //        System.out.printf("%s", data2);
 //        }
-//    23번문제
-//    float a = sc.nextFloat();
-//    System.out.printf("%f", a);
+//    23번문제 ~ 30번문제 x
+
+//23번
+//  public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    String[] a = sc.nextLine().split("\\.");
+//    int b = Integer.parseInt(a[0]);
+//    int c = Integer.parseInt(a[1]);
+//    System.out.printf("%d\n%d", b, c);
+//  }
+//}
+// 26번
+//public static void main(String[] args) {
+//  Scanner sc = new Scanner(System.in);
+//  String[] inputStr = sc.nextLine().split("\\:");
+//  int outPut1 = Integer.parseInt(inputStr[0]);
+//  int outPut2 = Integer.parseInt(inputStr[1]);
+//  int outPut3 = Integer.parseInt(inputStr[2]);
+//  System.out.printf("%d", outPut2);
+//}
+//}
+// 27번
+//  public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    String[] inputStr = sc.nextLine().split("\\.");
+//    int outPut1 = Integer.parseInt(inputStr[0]);
+//    int outPut2 = Integer.parseInt(inputStr[1]);
+//    int outPut3 = Integer.parseInt(inputStr[2]);
+//    System.out.printf("%02d-%02d-%04d", outPut3, outPut2, outPut1);
+//  }
+//}
+
+
+//    31번문제 10진 정수 1개 입력받아 8진수로 출력하기(설명)
+//  Scanner sc = new Scanner(System.in);
+//      int a = sc.nextInt();
+//      System.out.printf("%o", a);
+
+//    32번문제 10진 정수 입력받아 16진수로 출력하기1(설명)
+//  Scanner sc = new Scanner(System.in);
+//      int a = sc.nextInt();
+//      System.out.printf("%x", a);
+
+//    33번문제 10진 정수 입력받아 16진수로 출력하기2(설명)
+//  Scanner sc = new Scanner(System.in);
+//      int a = sc.nextInt();
+//      System.out.printf("%X", a);
+
+//    34번문제 8진 정수 1개 입력받아 10진수로 출력하기(설명)
+//    Scanner sc = new Scanner(System.in);
+//        String octal = sc.next();
+//        int decimal = Integer.parseInt(octal, 8);
+//        System.out.println(decimal);
+
+//    35번문제 16진 정수 1개 입력받아 8진수로 출력하기(설명)
+//    Scanner sc = new Scanner(System.in);
+//        String octal = sc.next();
+//        int decimal = Integer.parseInt(octal, 16);
+//        System.out.printf("%o", decimal);
+
+//    36번문제 [기초-출력변환] 영문자 1개 입력받아 10진수로 출력하기(설명)
+//    Scanner sc = new Scanner(System.in);
+//        char octal = sc.next().charAt(0);
+//        int asci = (int) octal;
+//        System.out.printf("%d", asci);
+
+//    37번문제 [기초-출력변환] 정수 입력받아 아스키 문자로 출력하기
+//    Scanner sc = new Scanner(System.in);
+//        int desimal = sc.nextInt();
+//        char asci = (char) desimal;
+//        System.out.printf("%c", asci);
+//    38번문제 정수 2개 입력받아 합 출력하기1(설명)
+//    Scanner sc = new Scanner(System.in);
+//        long first = sc.nextLong();
+//        long second = sc.nextLong();
+//        System.out.println(first + second);
+
+//    39번문제
+//Scanner sc = new Scanner(System.in);
+//long first = sc.nextLong();
+//  long second = sc.nextLong();
+//  System.out.println(first + second);
+
+// 39번
+//    Scanner sc = new Scanner(System.in);
+//    String[] arrStr = sc.nextLine().split(" ");
+//    long input1 = Long.parseLong(arrStr[0]);
+//    long input2 = Long.parseLong(arrStr[1]);
+//    System.out.println(input1+input2);
+
+//40번
+//    Scanner sc = new Scanner(System.in);
+//    long a = sc.nextLong();
+//    System.out.printf("%d", -a);
+
+//    41번문제
+//    Scanner sc = new Scanner(System.in);
+//    char c = sc.next().charAt(0);
+//    int asci = (int) c+1;
+//    System.out.printf("%c", asci);
+//    42번문제
+//    Scanner sc = new Scanner(System.in);
+//    int a = sc.nextInt();
+//    int b = sc.nextInt();
+//    int outputNum = a/b;
+//    System.out.printf("%d", outputNum);
