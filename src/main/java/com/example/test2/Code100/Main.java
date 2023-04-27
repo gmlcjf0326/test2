228fc8c0d20ca74660366965d59b69cbf0e46306
김희철 <gmlcjf0326@hanmail.net>
2023-04-27 11:24:42 +0900

    feat : codeUP ~1052 clear

--- src/main/java/com/example/test2/Code100/Main.java
+++ src/main/java/com/example/test2/Code100/Main.java
@@ -6,8 +6,8 @@ public class Main {
   public static void main(String[] args) {
 //    52번문제
 //    Scanner sc = new Scanner(System.in);
-//    int a = sc.nextInt();
-//    int b = sc.nextInt();
+//    int inputA = sc.nextInt();
+//    int inputb = sc.nextInt();
 //    System.out.printf("%d\n", a != b ? 1 : 0);
   }
 }
